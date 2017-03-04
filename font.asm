e997b3bfe2e5d060913aacea1da93f03781b0fdb
Cesar Gimenes <crg@crg.eti.br>
2017-03-04 15:41:13 -0300

    font example

--- font.asm
+++ font.asm
@@ -3,74 +3,39 @@
     org 100h
 section .text
 
-start:
-	push	ds		;
-	pop	es			; make sure ES = DS
-	
-	;mov ah,00h
-	;mov al,0Eh
-	;int 10h
+start:  
+        ;mov  ax,0003h      ; set screen mode to normal
+        ;int  10h           ; (80x25) text
 
-    ;mov  ax,0003h   ; set screen mode to normal
-    ;int  10h        ;  (80x25) text
-
-
-	mov	bp, OurFont;
-	mov	cx,02			; we'll change just 2 of them
-	mov	dl,65			;   A and B --> our A and B
-	xor dh,dh
-	mov	bh,16			; 16 bytes per char
-	mov	bl,0			; RAM block
-	mov	ax,1100h		; change font to our font
-	int	10h			; video interrupt
-	mov	ax,4C00h		; exit to DOS
-	int	21h			;
+	    push ds		        ;
+    	pop es			    ; make sure ES = DS
+    	mov	bp,font         ;
+	    mov	cx,1			; change 1 bitmap
+    	mov	dx,0065			; A
+	    mov	bx,1000h		; bh 16 bl 00 -> 16 bytes per char RAM block 00
+	    mov	ax,1100h		; change font to our font
+	    int	10h			    ; video interrupt
+    	mov	ax,4C00h		; exit to DOS
+	    int	21h			    ;
 
 section .data
-OurFont:	
-	db	00000000b
-	db	11111111b
-    db  00000000b
-    db  11111111b
-    db  00000000b
-    db  11111111b
-    db  00000000b
-    db  11111111b
-    db  00000000b
-    db  11111111b
-    db  00000000b
-    db  11111111b
-    db  00000000b
-    db  11111111b
-    db  00000000b
-    db  11111111b
-    db  00000000b
-    db  11111111b
-    db  00000000b
-    db  11111111b
-    db  00000000b
-    db  11111111b
-    db  00000000b
-    db  11111111b
-    db  00000000b
-    db  11111111b
-    db  00000000b
-    db  11111111b
-    db  00000000b
-    db  11111111b
-    db  00000000b
-    db  11111111b
-    db  00000000b
-    db  11111111b
-    db  00000000b
-    db  11111111b
-    db  00000000b
-    db  11111111b
-
-section .bss 
-
-        ; put uninitialized data here
+font:	
+	    db	00000000b       ; 1
+	    db	01111100b       ; 2
+	    db	11111110b       ; 3
+	    db	11000010b       ; 4
+	    db	11000010b       ; 5
+	    db	11000010b       ; 6
+	    db	11000010b       ; 7
+	    db	11111110b       ; 8
+	    db	11000010b       ; 9
+	    db	11000010b       ; 10
+	    db	11000010b       ; 11
+	    db	11000010b       ; 12
+	    db	11000010b       ; 13
+	    db	11000010b       ; 14
+	    db	00000000b       ; 15
+	    db	00000000b       ; 16
 
 
-.end				; End of assembly code
-
+.end
